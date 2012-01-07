0b3f9438c41b8480b5e646975087322cd6371434
Daniele Poggi <daniele@toodev.com>
2012-01-07 12:00:32 +0100

    first commit

---
+++ TDMulticast.h
@@ -0,0 +1,13 @@
+//
+//  TDMulticast.h
+//  TooConsole
+//
+//  Created by Daniele Poggi on 1/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TDMulticast : NSObject
+
+@end
---
+++ TDMulticast.m
@@ -0,0 +1,13 @@
+//
+//  TDMulticast.m
+//  TooConsole
+//
+//  Created by Daniele Poggi on 1/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "TDMulticast.h"
+
+@implementation TDMulticast
+
+@end
